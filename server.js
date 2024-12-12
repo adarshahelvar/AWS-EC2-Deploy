1b006ea349c724963b07bbb341be44a074939df1
Adarsha A <108178521+adarshahelvar@users.noreply.github.com>
2024-12-12 16:05:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,7 @@
 // Import Express module
-const express = require('express');
+var http = require('http');
 
-// Initialize Express app
-const app = express();
-
-// Define the server port
-const PORT = 3000;
-
-// Define the root route
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-// Handle 404 for other routes
-app.use((req, res) => {
-    res.status(404).send('Not Found');
-});
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-});
+http.createServer(function(req, res){
+    res.write("Hello from EC2 server");
+    res.end();
+}).listen(80);
